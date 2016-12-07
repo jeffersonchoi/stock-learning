d40a38f8a5ab4693549c8a48378b0c7561973a84
Jefferson Choi <jeffersonchoi@gmail.com>
2016-12-06 16:28:09 -0800

    redirect to user page after sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,8 +50,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-	users_path(resouce)
-    super(resource)
+	user_path(resource)
   end
 
   # The path used after sign up for inactive accounts.
