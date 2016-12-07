efb8f7683bc3663ee7996e29f7bad04c5fc253e2
Jefferson Choi <jeffersonchoi@gmail.com>
2016-12-06 16:32:20 -0800

    changing path for direct sign in sign up and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'sign_up' }
   resources :users, :only => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
